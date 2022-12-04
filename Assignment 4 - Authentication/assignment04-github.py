b2b9852addd60f072bd53db3db4102a2f061d1e4
Przemyslaw Bil <g00398317@gmit.ie>
2022-12-04 18:52:07 +0000

    Displayed content of private repo using apikey

--- Assignment 4 - Authentication/assignment04-github.py
+++ Assignment 4 - Authentication/assignment04-github.py
@@ -1,1 +1,30 @@
 # DATA REPRESENTATION Assignment 04
+
+import requests
+import json
+from urllib.parse import urljoin
+from config import config
+
+apiKey = config["myKey"]
+
+owner = "przemekbil"
+repository = "private_rep"
+#repository = "data-representation-courseware"
+
+apiUrl =  urljoin("https://api.github.com/repos/", owner + "/" + repository + "/contents")
+print(apiUrl)
+
+# get the content of the repository
+response = requests.get(apiUrl, auth=('token', apiKey))
+
+content = response.json()
+
+# print filenames in the repository
+for repo_element in content:
+    print(repo_element["name"])
+
+# write to file
+#with open("github.json", "w") as outfile:
+#    json.dump(content, outfile, indent=4)
+
+#print(response.json())
